395a59678b785340d22c1cb688e3e141ecec7ef8
LEON <kjl7966@gmail.com>
2025-07-10 01:17:15 +0800

    minor fixes

--- src/main/java/com/leon/rest_api/controller/CommonApiController.java
+++ src/main/java/com/leon/rest_api/controller/CommonApiController.java
@@ -1,20 +1,15 @@
 package com.leon.rest_api.controller;
 
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
-import java.util.HashMap;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.leon.rest_api.service.UserInfoInquiry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.web.bind.annotation.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommonApiController {
